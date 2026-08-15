953f65c72f7e9ba0ab6a897322dca5036cd75d0f
shakhawat-ily <420420shakhawat@gmail.com>
2026-08-15 21:33:18 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-const SUPABASE_URL = "https://vpjadizr8kobvdfjqxws.supabase.co";
-
+const SUPABASE_URL = "https://vpjadirzokbvdbfjqxws.supabase.co";
 const SUPABASE_KEY = "sb_publishable_Yu_SQZRYGFPhWDg_G_V4xw_Nm5lBQuM";
